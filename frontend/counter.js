419ad69a2bdf5568b7a0b9902fb74f19e697f241
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-09-06 16:37:04 -0600

    added localStorage logic

--- frontend/counter.js
+++ frontend/counter.js
@@ -1,19 +1,16 @@
 const counterElement = document.getElementById('visitor-count'); //gives counterElement the value of the span element belonging to "visitor-count" in the webpage. This is "locating the target".
-localStorage.setItem('visited', 'false');
-localStorage.setItem('count', 'null');
-
 
 // Fetch uses GET by default, so we also specify POST below.
-  if (localStorage.getItem('visited') != 'true') {
-    fetch('https://cdwy7ezni1.execute-api.us-west-2.amazonaws.com', { method: 'POST'}) //fetches the API and returns a promise
-        .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
-        .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.getItem('count') = count }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
+if (localStorage.getItem('visited') === null) {
+  fetch('https://cdwy7ezni1.execute-api.us-west-2.amazonaws.com', { method: 'POST'}) //fetches the API and returns a promise
+      .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
+      .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.setItem('count', count); localStorage.setItem('visited', 'true') }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
 
-        localStorage.setItem('visited', 'true');
-        //every HTML element has a .textContent property. It represents the visible text inside the element.
-  } else {
 
-  }
+      //every HTML element has a .textContent property. It represents the visible text inside the element.
+} else {
+  counterElement.textContent = localStorage.getItem('count');
+}
 
 
 
