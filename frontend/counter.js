cd9a012600e6efe17c49ee6c090dedb8bb94d928
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-09-06 16:10:18 -0600

    added extra logic for localStorage

--- frontend/counter.js
+++ frontend/counter.js
@@ -1,10 +1,20 @@
-const counterElement = document.getElementById('visitor-count'); //gives counterElement the value of the id belonging to "visitor-count" in the webpage (the <span> element itself). This is "locating the target"
+const counterElement = document.getElementById('visitor-count'); //gives counterElement the value of the span element belonging to "visitor-count" in the webpage. This is "locating the target".
+localStorage.setItem('visited', 'false');
+localStorage.setItem('count', 'null');
 
-fetch('https://somethingamazon') //fetches the API and returns a promise
-    .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
-    .then(data => { const count = data.visits; counterElement.textContent = count; }); //takes the response.json as data and adds the value of data.visits to constant count
 
-    //every HTML element has a .textContent property. It represents the visible text inside the element.
+// Fetch uses GET by default, so we also specify POST below.
+  if (localStorage.getItem('visited') != 'true') {
+    fetch('https://cdwy7ezni1.execute-api.us-west-2.amazonaws.com', { method: 'POST'}) //fetches the API and returns a promise
+        .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
+        .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.getItem('count') = count }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
+
+        localStorage.setItem('visited', 'true');
+        //every HTML element has a .textContent property. It represents the visible text inside the element.
+  } else {
+
+  }
+
 
 
 
