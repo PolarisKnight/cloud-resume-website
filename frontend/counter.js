e8349ea8f1f4c796ecdd001b3abcd2e28c6c9fcf
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-09-06 23:55:16 -0600

    made counter time-sensitive

--- frontend/counter.js
+++ frontend/counter.js
@@ -1,10 +1,11 @@
 const counterElement = document.getElementById('visitor-count'); //gives counterElement the value of the span element belonging to "visitor-count" in the webpage. This is "locating the target".
+const time = Date.now() // Date.now is number of ms elapsed since January 1, 1970
 
 // Fetch uses GET by default, so we also specify POST below.
-if (localStorage.getItem('visited') === null) {
+if (time - localStorage.getItem('storedTime') >= 8 * 60 * 60 * 1000) {
   fetch('https://cdwy7ezni1.execute-api.us-west-2.amazonaws.com', { method: 'POST'}) //fetches the API and returns a promise
       .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
-      .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.setItem('count', count); localStorage.setItem('visited', 'true') }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
+      .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.setItem('count', count); localStorage.setItem('storedTime', time) }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
 
 
       //every HTML element has a .textContent property. It represents the visible text inside the element.
