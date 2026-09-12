ebfc03ce8a874ae4c561cff577f7296c246e341f
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-09-12 16:04:28 -0600

    updated directory structure and added finishing touches to HTML

--- frontend/js/counter.js
+++ frontend/js/counter.js
@@ -2,7 +2,7 @@ const counterElement = document.getElementById('visitor-count'); //gives counter
 const time = Date.now() // Date.now is number of ms elapsed since January 1, 1970
 
 // Fetch uses GET by default, so we also specify POST below.
-if (time - localStorage.getItem('storedTime') >= 8 * 60 * 60 * 1000) {
+if (time - localStorage.getItem('storedTime') >= 8 * 60 * 60 * 1000) { //only update counter after 8 hours per visitor. This is to stop the counter from goping up by simply refreshing the page.
   fetch('https://cdwy7ezni1.execute-api.us-west-2.amazonaws.com', { method: 'POST'}) //fetches the API and returns a promise
       .then(response => response.json()) //result from the promise is used in the .then function to unwrap the json into a usable JS object
       .then(data => { const count = data.visits; counterElement.textContent = count; localStorage.setItem('count', count); localStorage.setItem('storedTime', time) }); //takes the response.json as data and adds the value of data.visits to constant count, then adds new value of count to counterElement.
